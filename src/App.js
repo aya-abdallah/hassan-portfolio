86f1e3335e91f67a58e370e5573f4b76ee592135
aya-abdullah <ayalaban888@yahoo.com>
2024-05-07 20:06:18 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Navigation } from './Components/Navigation/Navigation';
 import { Home } from './Components/Home/Home';
 
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, HashRouter } from 'react-router-dom';
 import Portfolio from './Components/Portfolio/Portfolio';
 import './App.css';
 import './GlobalStyles.scss'
@@ -10,12 +10,14 @@ function App() {
   return (
     <div className="App">
       <Navigation />
+      <HashRouter basename='/'>
+        <Routes >
 
-      <Routes baseline="/">
+          <Route path="/" exact element={<Home />} />
+          <Route path='/portfolio' element={<Portfolio />} />
+        </Routes>
+      </HashRouter>
 
-        <Route path="/" exact element={<Home />} />
-        <Route path='/portfolio' element={<Portfolio />} />
-      </Routes>
 
 
     </div>
